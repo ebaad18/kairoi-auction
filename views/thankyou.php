0260b8ec8cda4eb0e7cc5b5f3c67bcda511f0846
Ebaad Ansari <ebaadansari@gmail.com>
2020-11-01 20:09:31 +0530

    User assurance added snackbar to thankyou page 3

--- views/thankyou.php
+++ views/thankyou.php
@@ -32,7 +32,7 @@ if($part_of_url == 'bid'){
 
   var x = document.getElementById('snackbar');
   x.className = 'show';
-  setTimeout(function(){ x.className = x.className.replace('show', ''); }, 6000);
+  setTimeout(function(){ x.className = x.className.replace('show', ''); }, 4000);
   </script>";
 }    
 
